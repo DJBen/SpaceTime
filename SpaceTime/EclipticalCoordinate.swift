f6da8993118b0ee3d7f2a7eb87b1959c054e7aa0
Sihao Lu <lsh32768@gmail.com>
2017-12-26 22:21:47 -0800

    Add distance to ecliptical coordinate

--- SpaceTime/EclipticalCoordinate.swift
+++ SpaceTime/EclipticalCoordinate.swift
@@ -12,19 +12,22 @@ public struct EclipticalCoordinate {
 
     public let longitude: Double
     public let latitude: Double
+    public let distance: Double
 
     public let julianDay: JulianDay
 
-    public init(longitude: Double, latitude: Double, julianDay: JulianDay) {
+    public init(longitude: Double, latitude: Double, distance: Double, julianDay: JulianDay) {
         self.latitude = latitude
         self.longitude = longitude
         self.julianDay = julianDay
+        self.distance = distance
     }
 
     public init(equatorialCoordinate coord: EquatorialCoordinate, julianDay: JulianDay) {
         let eps = EclipticUtil.meanObliquityOfEcliptic(julianDay: julianDay)
         longitude = atan2(sin(coord.rightAscension) * cos(eps) + tan(coord.declination) * sin(eps), cos(coord.rightAscension))
         latitude = asin(sin(coord.declination) * cos(eps) - cos(coord.declination) * sin(eps) * sin(coord.rightAscension))
+        distance = coord.distance
         self.julianDay = julianDay
     }
 }
@@ -36,6 +39,6 @@ public extension EquatorialCoordinate {
         let eps = EclipticUtil.meanObliquityOfEcliptic(julianDay: julianDay)
         rightAscension = atan2(sin(coord.longitude) * cos(eps) - tan(coord.latitude) * sin(eps), cos(coord.longitude))
         declination = asin(sin(coord.latitude) * cos(eps) + cos(coord.latitude) * sin(eps) * sin(coord.longitude))
-        distance = 1
+        distance = coord.distance
     }
 }
